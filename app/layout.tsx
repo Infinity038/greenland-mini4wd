2cd201eff7330436cb52a3f40e6db35f827f5bff
Infinity038 <alltfarmer@gmail.com>
2026-05-18 17:44:17 -0100

    Restore dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
         />
       </head>
-      <body className="bg-[#F3F4F6] antialiased">{children}</body>
+      <body className="bg-[#0d0d0d] antialiased">{children}</body>
     </html>
   );
 }
